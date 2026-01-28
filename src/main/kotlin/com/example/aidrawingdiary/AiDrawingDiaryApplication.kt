38efb5c7b786757daebb0cfcb713519dc562e71c
khxxn <kyohyun58@naver.com>
2026-01-28 11:37:15 +0900

    first commit

---
+++ src/main/kotlin/com/example/aidrawingdiary/AiDrawingDiaryApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aidrawingdiary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiDrawingDiaryApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiDrawingDiaryApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/aidrawingdiary/AiDrawingDiaryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.aidrawingdiary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiDrawingDiaryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

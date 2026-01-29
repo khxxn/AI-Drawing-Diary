d91834205a91e7eafcb06fa5ca7ab58d3eb754a8
KooKyoHyun <kyohyun58@naver.com>
2026-01-29 14:23:17 +0900

    CORS

---
+++ src/main/kotlin/com/example/aidrawingdiary/WebConfig.kt
@@ -0,0 +1,22 @@
+package com.example.aidrawingdiary
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**") // 모든 URL
+                    .allowedOrigins("*") // 허용할 origin
+                    .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .allowCredentials(true)
+            }
+        }
+    }
+}
